372c1154df50b429c15ee8ceb281d9593d700a7c
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2020-04-15 23:48:56 +0900

    tmp

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <div class="group">
 
             <div class="site-footer__col-one">
-                <h1 class="school-logo-text school-logo-text--alt-color"><a href="#"><strong>Fictional</strong> University</a></h1>
+                <h1 class="school-logo-text school-logo-text--alt-color"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
                 <p><a class="site-footer__link" href="#">555.555.5555</a></p>
             </div>
 
@@ -14,10 +14,10 @@
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
                         <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Programs</a></li>
-                            <li><a href="#">Events</a></li>
-                            <li><a href="#">Campuses</a></li>
+                            <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+                            <li><a href="<?php echo site_url(); ?>">Programs</a></li>
+                            <li><a href="<?php echo site_url(); ?>">Events</a></li>
+                            <li><a href="<?php echo site_url(); ?>">Campuses</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -26,9 +26,9 @@
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
                         <ul>
-                            <li><a href="#">Legal</a></li>
-                            <li><a href="#">Privacy</a></li>
-                            <li><a href="#">Careers</a></li>
+                            <li><a href="<?php echo site_url(); ?>">Legal</a></li>
+                            <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
+                            <li><a href="<?php echo site_url(); ?>">Careers</a></li>
                         </ul>
                     </nav>
                 </div>
