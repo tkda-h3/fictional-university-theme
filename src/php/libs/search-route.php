349e85f0006ac0b15d19aea5d61fb2cc70f10503
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2021-08-25 00:33:49 +0900

    様々なpost_typeを検索対象に含める

--- src/php/libs/search-route.php
+++ src/php/libs/search-route.php
@@ -1,27 +1,49 @@
-<?php 
+<?php
 
-function my_register_search(){
+function my_register_search()
+{
   register_rest_route('univ/v1', 'search', array(
     'methods' => WP_REST_Server::READABLE, // GET
     'callback' => 'universitySearchResults',
   ));
 }
 
-function universitySearchResults($data) {
-  $professors = new WP_Query(array(
+function universitySearchResults($data)
+{
+  $main_query = new WP_Query(array(
     's' => sanitize_text_field($data['term']),
-    'post_type' => 'professor'
+    'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
   ));
 
-  $professorResults = array();
-  while($professors->have_posts()) {
-    $professors->the_post();
-    array_push($professorResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink(),
-    ));
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array(),
+  );
+
+  $post_type_to_result_key = array(
+    'post' => 'generalInfo',
+    'page' => 'generalInfo',
+    'professor' => 'professors',
+    'program' => 'programs',
+    'event' => 'events',
+    'campus' => 'campuses',
+  );
+
+  while ($main_query->have_posts()) {
+    $main_query->the_post();
+
+    array_push(
+      $results[$post_type_to_result_key[get_post_type()]],
+      array( 
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ),
+    );
   }
-  return $professorResults;
+  return $results;
 }
 
 add_action('rest_api_init', 'my_register_search');
