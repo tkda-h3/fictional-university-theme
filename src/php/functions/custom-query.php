13dd93d499ba1a0a8cefa54fb87b8b0af57a0b8c
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2021-09-01 15:51:33 +0900

    イベントの年月アーカイブリンク

--- src/php/functions/custom-query.php
+++ src/php/functions/custom-query.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // メインクエリの書き換え
 function my_pre_get_posts($query)
 {
@@ -36,10 +36,10 @@ add_action('pre_get_posts', 'my_pre_get_posts');
 // note post を強制的に非公開
 // insert以外に、updateでも呼ばれるhook
 add_filter('wp_insert_post_data', function ($data, $postarr) {
-	if($data['post_type'] == 'note'){
+	if ($data['post_type'] == 'note') {
 		$note_exist = !$postarr['ID'];
 		// updateの場合のみ
-		if(count_user_posts(get_current_blog_id(), 'note') >= 5 and $note_exist){
+		if (count_user_posts(get_current_blog_id(), 'note') >= 5 and $note_exist) {
 			die("You have reached max limit.");
 		}
 
@@ -54,4 +54,46 @@ add_filter('wp_insert_post_data', function ($data, $postarr) {
 }, 10, 2);
 
 
-?>
+
+
+function my_eventt_query($query)
+{
+	if (!is_admin() and $query->is_date()) {
+		$query_object  = get_queried_object();
+		$the_post_type = $query_object->name;
+		if ('event' === $the_post_type) {
+			if ($query->is_year()) {
+				$year = get_query_var('year');
+				$from = date($year . '-01-01');
+				$to   = date($year . '-12-31');
+			} elseif ($query->is_month()) {
+				$year  = get_query_var('year');
+				$month = get_query_var('year') . '-' . get_query_var('monthnum');
+				$from  = date('Y-m-d', strtotime('first day of ' . $month));
+				$to    = date('Y-m-d', strtotime('last day of ' . $month));
+			} elseif ($query->is_day()) {
+				$year  = get_query_var('year');
+				$month = get_query_var('monthnum');
+
+				$day   = get_query_var('day');
+				$from  = date($year . '-' . $month . '-' . $day);
+				$to    = date($year . '-' . $month . '-' . $day);
+			}
+			$meta_query = array(
+				array(
+					'key'     => 'event_date',
+					'value'   => array($from, $to),
+					'compare' => 'BETWEEN',
+					'type'    => 'DATE',
+				),
+			);
+			$query->set('meta_key', 'event_date');
+			$query->set('orderby', 'meta_value');
+			$query->set('year', '');
+			$query->set('monthnum', '');
+			$query->set('day', '');
+			$query->set('meta_query', $meta_query);
+		}
+	}
+}
+add_action('pre_get_posts', 'my_eventt_query');
